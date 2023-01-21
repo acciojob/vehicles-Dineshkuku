94af1fdcb63761e7ad2ef101ccfb01bdf573b540
Dinesh Kukreja <dhirensharma540@gmail.com>
2023-01-21 20:01:55 +0530

    Updated src

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -22,6 +22,11 @@ public class Car extends Vehicle {
         currentGear = 1;
     }
 
+    public void changeGear(int newGear){
+        this.currentGear = newGear;
+        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+    }
+
     public int getWheels() {
         return wheels;
     }
@@ -50,11 +55,7 @@ public class Car extends Vehicle {
         return seats;
     }
 
-    public void changeGear(int newGear){
-        this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
-    }
-
+    
     public void changeSpeed(int newSpeed, int newDirection){
         move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
